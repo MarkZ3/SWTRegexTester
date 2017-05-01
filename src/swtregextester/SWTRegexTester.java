5a7119abbcc1f24a459a13567d37ed145ba29808
Marc-Andre Laperle <malaperle@gmail.com>
2017-05-01 15:47:24 -0400

    Some group stuff
    Signed-off-by: Marc-Andre Laperle <malaperle@gmail.com>

--- src/swtregextester/SWTRegexTester.java
+++ src/swtregextester/SWTRegexTester.java
@@ -25,18 +25,20 @@ public class SWTRegexTester {
 	public static void main(String[] args) {
 		new SWTRegexTester().run();		
 	}
+	
+	static final int nbResults = 10;
 
 	private Text regexpText;
 	private Text testText;
 	private Label result;
-	private Text findText;
-	private Text matchText;
+	private Text[] findText = new Text[nbResults];
+	private Text[] groupText = new Text[nbResults];;
 
 	private void run() {
 
 		final Display display = new Display();
 		Shell shell = new Shell(display);
-		GridLayout layout = new GridLayout(2, false);
+		GridLayout layout = new GridLayout(4, false);
 		shell.setLayout(layout);
 
 		final Label regexpLbl = new Label(shell, SWT.NONE);
@@ -44,6 +46,7 @@ public class SWTRegexTester {
 
 		regexpText = new Text(shell, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
 		GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gd.horizontalSpan = 3;
 		regexpText.setLayoutData(gd);
 		regexpText.addKeyListener(new KeyAdapter() {
 			@Override
@@ -68,6 +71,7 @@ public class SWTRegexTester {
 		testLbl.setText("Test");
 		testText = new Text(shell, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL);
 		gd = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gd.horizontalSpan = 3;
 		testText.setLayoutData(gd);
 		testText.addKeyListener(new KeyAdapter() {
 			@Override
@@ -92,27 +96,37 @@ public class SWTRegexTester {
 		resultLbl.setText("Result:");
 		result = new Label(shell, SWT.NONE);
 		gd = new GridData(SWT.FILL, SWT.FILL, true, false);
+		gd.horizontalSpan = 4;
 		result.setLayoutData(gd);
 		result.setText("none");
 		Button b = new Button(shell, SWT.PUSH);
 		b.setText("Test");
 		gd = new GridData(SWT.FILL, SWT.FILL, true, false);
-		gd.horizontalSpan = 2;
+		gd.horizontalSpan = 4;
 		b.setLayoutData(gd);
 		
-		final Label matchLbl = new Label(shell, SWT.NONE);
-		matchLbl.setText("Match");
+//		final Label matchLbl = new Label(shell, SWT.NONE);
+//		matchLbl.setText("Match");
 		
-		matchText = new Text(shell, SWT.BORDER);
-		gd = new GridData(SWT.FILL, SWT.FILL, true, false);
-		matchText.setLayoutData(gd);
-		
-		final Label groupLbl = new Label(shell, SWT.NONE);
-		groupLbl.setText("Find 0");
-		
-		findText = new Text(shell, SWT.BORDER);
-		gd = new GridData(SWT.FILL, SWT.FILL, true, false);
-		findText.setLayoutData(gd);
+//		matchText = new Text(shell, SWT.BORDER);
+//		gd = new GridData(SWT.FILL, SWT.FILL, true, false);
+//		matchText.setLayoutData(gd);
+//		
+		for (int i = 0; i < nbResults; i++) {
+			Label groupLbl = new Label(shell, SWT.NONE);
+			groupLbl.setText("Find " + i);
+
+			findText[i] = new Text(shell, SWT.BORDER);
+			gd = new GridData(SWT.FILL, SWT.FILL, true, false);
+			findText[i].setLayoutData(gd);
+			
+			groupLbl = new Label(shell, SWT.NONE);
+			groupLbl.setText("Group " + i);
+
+			groupText[i] = new Text(shell, SWT.BORDER);
+			gd = new GridData(SWT.FILL, SWT.FILL, true, false);
+			groupText[i].setLayoutData(gd);
+		}
 		
 		b.addSelectionListener(new SelectionListener() {
 
@@ -168,25 +182,41 @@ public class SWTRegexTester {
 		String text2 = testText.getText();
 		Matcher matcher = compile.matcher(text2);
 		boolean matches = matcher.matches();
+		boolean find = false;
+		printGroups(matches, matcher);
 		if (matches) {
-			matchText.setText(matcher.group(0));
-		} else {
-			matchText.setText("");
+		} else {			
+			matcher.reset();
+			for (int j = 0; j < nbResults; j++) {
+				boolean findCurrent = matcher.find();
+				result.setText(Boolean.toString(find));
+				if (findCurrent) {
+					findText[j].setText(matcher.group(0));
+					if (j == 0) {
+						printGroups(true, matcher);
+					}
+				} else {
+					findText[j].setText("");
+				}
+				find = find || findCurrent;
+			}
 		}
 		
-		matcher.reset();
-		boolean find = matcher.find();
-		result.setText(Boolean.toString(find));
-		if (find) {
-			findText.setText(matcher.group(0));
-		} else {
-			findText.setText("");
-		}
 		
 		if (find || matches) {
 			result.setBackground(display.getSystemColor(SWT.COLOR_GREEN));
 		} else {
 			result.setBackground(display.getSystemColor(SWT.COLOR_RED));
+		}
+	}
+
+	void printGroups(boolean matches, Matcher matcher) {
+		for (int j = 0; j < nbResults; j++) {
+			if (matches && j <= matcher.groupCount() && matcher.group(j) != null) {
+				groupText[j].setText(matcher.group(j));
+			} else {
+				groupText[j].setText("");
+			}
 		}
 	}
 
